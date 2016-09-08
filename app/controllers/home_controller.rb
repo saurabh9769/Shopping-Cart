0e9fd1b93831e884d70af26e961223e939b35ddd
Saurabh Sangani <sanganisaurabh92@gmail.com>
2016-09-08 09:46:42 +0530

    Changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@
 		@categories = Category.where( parent_id: nil)
 		@products = Product.per_page_kaminari(params[:page]).per(3)
 		@products = @products.where(category_id: params[:category_id]) if params[:category_id].present?
-		@cart = session[:product_ids].count
+		@cart = session[:product_ids].count if params[:product_id].present?
 	end
 
 
